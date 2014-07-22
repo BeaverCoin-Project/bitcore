9cf0905244f2364bf909280ce49e879c4d2b56f0
nodecode <info@nodecode.de>
2014-07-22 17:53:14 +0200

    Update networks.js

--- networks.js
+++ networks.js
@@ -13,7 +13,7 @@ exports.livenet = {
   hkeyPublicVersion: 0x0488b21e,
   hkeyPrivateVersion: 0x0488ade4,
   genesisBlock: {
-    hash: hex('4daea126edbcc5657db163e07e7ca44ffa095135599e2a9d1c73a125c539515f'),
+    hash: hex('5f5139c525a1731c9d2a9e59355109fa4fa47c7ee063b17d65c5bced26a1ae4d'),
     merkle_root: hex('5c8ac8f25d3325c785b75e6cc35258fe01cd7d42f29eb60b22f10490d21ebc16'),
     height: 0,
     nonce: 1080338,
